176b5309b5577f07e129525588c2cb4fb9cffc8f
yumna-s <fethiya945@gmail.com>
2025-03-29 18:19:35 +0300

    fix bug

--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -25,32 +25,42 @@ export const TodoWrapperLocalStorage = () => {
       )
     );
   };
-  const editTodo = (id) => {
-    setTodos(
-      todos.map((todo) =>
-        todo.id === id ? { ...todo, isEditing: !todo.isEditing } : todo
+};
+const editTodo = (id) => {
+  setTodos(
+    todos.map((todo) =>
+      todo.id === id ? { ...todo, isEditing: !todo.isEditing } : todo
+    )
+  );
+};
+const editTask = (task, id) => {
+  setTodos(
+    todos.map((todo) =>
+      todo.id === id ? { ...todo, task, isEditing: !todo.isEditing } : todo
+    )
+  );
+};
+
+const deleteTodo = (id) => {
+  setTodos(todos.filter((todo) => todo.id !== id));
+};
+return (
+  <div className="TodoWrapper">
+    <h1>Get Things Done !</h1>
+    <TodoForm addTodo={addTodo} />
+    {/* display todos */}
+    {todos.map((todo) =>
+      todo.isEditing ? (
+        <EditTodoForm editTodo={editTask} task={todo} />
+      ) : (
+        <Todo
+          key={todo.id}
+          task={todo}
+          deleteTodo={deleteTodo}
+          editTodo={editTodo}
+          toggleComplete={toggleComplete}
+        />
       )
-    );
-  };
-  const editTask = (task, id) => {
-    setTodos(
-      todos.map((todo) =>
-        todo.id === id ? { ...todo, task, isEditing: !todo.isEditing } : todo
-      )
-    );
-  };
-  return (
-    <div className="TodoWrapper">
-      <h1>Get Things Done !</h1>
-      <TodoForm addTodo={addTodo} />
-      {/* display todos */}
-      {todos.map((todo) =>
-        todo.isEditing ? (
-          <EditTodoForm editTodo={editTask} task={todo} />
-        ) : (
-          <Todo
-            <TodoForm addTodo={addTodo} />
-      {/* display todos */}
-      {todos.map((todo) =>
-        todo.isEditing ? (
-          <EditTodoForm editTodo={editTask} task={todo} />
+    )}
+  </div>
+);
