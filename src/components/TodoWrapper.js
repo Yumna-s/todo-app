6a6fd5df0dbb2acfcafbf8d5b2c5bc234104f2dd
yumna-s <fethiya945@gmail.com>
2025-03-29 15:44:38 +0300

    adding use effect

---
+++ src/components/TodoWrapper.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from "react";
+import { TodoForm } from "./TodoForm";
+import { v4 as uuidv4 } from "uuid";
+import { Todo } from "./Todo";
+import { EditTodoForm } from "./EditTodoForm";
+uuidv4();
+
+export const TodoWrapperLocalStorage = () => {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    const savedTodos = JSON.parse(localStorage.getItem("todos")) || [];
+    setTodos(savedTodos);
+  }, []);
+};
